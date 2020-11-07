44a49c36ee8b5b5441843968d35f5e745b9f8f3c
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-11-07 14:44:38 +0900

    Implement header, but without score

--- ckka_rust/src/lib.rs
+++ ckka_rust/src/lib.rs
@@ -16,19 +16,23 @@ pub enum HeaderElem {
     KeyedValue(String, String),
 }
 
-pub fn parse_header(s: &str) -> Header {
-    unimplemented!()
-}
-
 use nom::bytes::complete::tag;
 use nom::bytes::complete::take_until;
 use nom::character::complete::*;
 use nom::multi::many0;
+use nom::multi::many_m_n;
 use nom::IResult;
 
-pub fn parse_braced_string(s: &str, open: char, close: char) -> IResult<&str, &str> {
+use std::collections::HashMap;
+
+pub fn parse_braced_string(
+    s: &str,
+    open_close: HashMap<char, char>,
+) -> IResult<&str, (&str, (char, char))> {
     let (no_used, vec) = many0(char('#'))(s)?;
-    let (no_used, _) = char(open)(no_used)?;
+    let (no_used, open) = one_of(&*open_close.keys().collect::<String>())(no_used)?;
+
+    let close = *open_close.get(&open).unwrap();
 
     // `}####` if vec.len() == 4
     let end_pattern = format!(
@@ -39,11 +43,16 @@ pub fn parse_braced_string(s: &str, open: char, close: char) -> IResult<&str, &s
     let (no_used, in_string) = take_until(&*end_pattern)(no_used)?;
     let (no_used, _) = tag(&*end_pattern)(no_used)?;
 
-    Ok((no_used, in_string))
+    let (no_used, _) = many0(one_of("\t\r\n \u{00a0}\u{3000}"))(no_used)?;
+
+    Ok((no_used, (in_string, (open, close))))
 }
 
 pub fn header_elem_parser(s: &str) -> IResult<&str, HeaderElem> {
-    let (no_used, in_string) = parse_braced_string(s, '{', '}')?;
+    let (no_used, (in_string, (open, close))) =
+        parse_braced_string(s, maplit::hashmap! {'{' => '}'})?;
+    assert_eq!(open, '{');
+    assert_eq!(close, '}');
     Ok((no_used, {
         let mut splitter = in_string.splitn(2, ':');
         let first = splitter.next().unwrap();
@@ -55,66 +64,36 @@ pub fn header_elem_parser(s: &str) -> IResult<&str, HeaderElem> {
     }))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use big_s::S;
-    #[test]
-    fn header_elem() {
-        assert_eq!(
-            header_elem_parser("{律:硬皇力}"),
-            Ok(("", HeaderElem::KeyedValue(S("律"), S("硬皇力"))))
-        )
-    }
+pub fn player_and_point_parser(s: &str) -> IResult<&str, PlayerAndPoint> {
+    // TODO implement parsing point
+    let (no_used, (player_name, (open, close))) =
+        parse_braced_string(s, maplit::hashmap! {'[' => ']'})?;
+    assert_eq!(open, '[');
+    assert_eq!(close, ']');
 
-    #[test]
-    fn header_elem_2() {
-        assert_eq!(
-            header_elem_parser("{硬皇力}"),
-            Ok(("", HeaderElem::Value(S("硬皇力"))))
-        )
-    }
-
-    #[test]
-    fn header_elem_3() {
-        assert_eq!(
-            header_elem_parser("#{硬皇力}#"),
-            Ok(("", HeaderElem::Value(S("硬皇力"))))
-        )
-    }
-
-    #[test]
-    fn header_elem_4() {
-        assert_eq!(
-            header_elem_parser("#{硬皇}力}#"),
-            Ok(("", HeaderElem::Value(S("硬皇}力"))))
-        )
-    }
-
-    #[test]
-    fn it_works() {
-        assert_eq!(
-            parse_header(
-                r#"{律:硬皇力}
-{2018年4月8日 18:00頃}
-[SY] [補集合]"#
-            ),
-            Header {
-                info: vec![
-                    HeaderElem::KeyedValue(S("律"), S("硬皇力")),
-                    HeaderElem::Value(S("2018年4月8日 18:00頃"))
-                ],
-                players: Some((
-                    PlayerAndPoint {
-                        player_name: S("SY"),
-                        point: 20
-                    },
-                    PlayerAndPoint {
-                        player_name: S("補集合"),
-                        point: 20
-                    }
-                ))
-            }
-        )
-    }
+    Ok((
+        no_used,
+        PlayerAndPoint {
+            player_name: player_name.to_owned(),
+            point: 20, /* FIXME */
+        },
+    ))
 }
+
+pub fn header_parser(s: &str) -> IResult<&str, Header> {
+    let (no_used, _) = many0(one_of("\t\r\n \u{00a0}\u{3000}"))(s)?;
+    let (no_used, info) = many0(header_elem_parser)(no_used)?;
+
+    let (no_used, vec2) = many_m_n(0, 2, player_and_point_parser)(no_used)?;
+
+    let players = match &vec2.as_slice() {
+        &[] => None,
+        &[a, b] => Some((a.clone(), b.clone())),
+        _ => panic!("only one player!"),
+    };
+
+    Ok((no_used, Header { info, players }))
+}
+
+#[cfg(test)]
+mod tests;
---
+++ ckka_rust/src/tests.rs
@@ -0,0 +1,64 @@
+use super::*;
+use big_s::S;
+
+#[test]
+fn header_elem() {
+    assert_eq!(
+        header_elem_parser("{律:硬皇力}"),
+        Ok(("", HeaderElem::KeyedValue(S("律"), S("硬皇力"))))
+    )
+}
+
+#[test]
+fn header_elem_2() {
+    assert_eq!(
+        header_elem_parser("{硬皇力}"),
+        Ok(("", HeaderElem::Value(S("硬皇力"))))
+    )
+}
+
+#[test]
+fn header_elem_3() {
+    assert_eq!(
+        header_elem_parser("#{硬皇力}#"),
+        Ok(("", HeaderElem::Value(S("硬皇力"))))
+    )
+}
+
+#[test]
+fn header_elem_4() {
+    assert_eq!(
+        header_elem_parser("#{硬皇}力}#"),
+        Ok(("", HeaderElem::Value(S("硬皇}力"))))
+    )
+}
+
+#[test]
+fn it_works() {
+    assert_eq!(
+        header_parser(
+            r#"{律:硬皇力}
+{2018年4月8日 18:00頃}
+[SY] [補集合]"#,
+        ),
+        Ok((
+            "",
+            Header {
+                info: vec![
+                    HeaderElem::KeyedValue(S("律"), S("硬皇力")),
+                    HeaderElem::KeyedValue(S("2018年4月8日 18"),S("00頃"))
+                ],
+                players: Some((
+                    PlayerAndPoint {
+                        player_name: S("SY"),
+                        point: 20
+                    },
+                    PlayerAndPoint {
+                        player_name: S("補集合"),
+                        point: 20
+                    }
+                ))
+            }
+        ))
+    )
+}
