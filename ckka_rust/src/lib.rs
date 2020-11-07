d849d24acd0072561662ef738fd5d22d446c0e0b
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-11-08 08:37:22 +0900

    Start parsing the body

--- ckka_rust/src/lib.rs
+++ ckka_rust/src/lib.rs
@@ -25,6 +25,43 @@ use nom::IResult;
 
 mod pekzep_numeral;
 
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
+
+type CKKA = (Header, String);
+
+pub fn parse_ckka(s: &str) -> Result<CKKA, ()> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r#"^\s*[KLNTZXCMP"]"#).unwrap();
+    }
+
+    let mut header = String::new();
+    let mut body = String::new();
+    let mut is_body = false;
+    for l in s.lines() {
+        if RE.is_match(l) {
+            is_body = true;
+        }
+
+        if is_body {
+            body.push_str(&mut l.clone());
+            body.push('\n');
+        } else {
+            header.push_str(&mut l.clone());
+            header.push('\n');
+        }
+    }
+
+    match header_parser(&header) {
+        Ok(("", parsed_head)) => Ok((parsed_head, body)),
+        Ok((a, _)) => panic!("Unparsable fragment `{}` left", a),
+        Err(_) => Err(())
+    }
+}
+
 pub fn parse_pekzep_numeral(s: &str) -> IResult<&str, i64> {
     let (no_used, vec) =
         many_m_n(1, 1000, one_of("無下一二三四五六七八九十百万億"))(s)?;
@@ -49,7 +86,7 @@ pub fn parse_braced_string(s: &str, open: char, close: char) -> IResult<&str, &s
 
     let (no_used, _) = many0(one_of("\t\r\n \u{00a0}\u{3000}"))(no_used)?;
 
-    if in_string.contains("\n") || in_string.contains("\r")  {
+    if in_string.contains("\n") || in_string.contains("\r") {
         panic!("neither key nor value in the header can contain a newline");
     }
 
