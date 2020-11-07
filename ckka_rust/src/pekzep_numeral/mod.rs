82eab4118cf171c1223ed46207956076f71a8050
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-11-07 17:26:22 +0900

    numbers

--- ckka_rust/src/pekzep_numeral/mod.rs
+++ ckka_rust/src/pekzep_numeral/mod.rs
@@ -48,8 +48,31 @@ fn less_than_100_nun1_elided(s: &[char]) -> Option<i64> {
             let d1 = less_than_10(*digit1)?;
             let d2 = less_than_10(*digit2)?;
             Some(d1 * 10 + d2)
-        },
+        }
         _ => None,
+    }
+}
+
+fn less_than_100000000(s: &[char]) -> Option<i64> {
+    match s {
+        ['万'] => Some(10000),
+        [head @ .., '万'] => less_than_10000(head).map(|w| w * 10000),
+        ['万', tail @ ..] => Some(10000 + less_than_10000(tail)?),
+
+        [a, '万', tail @ ..] => Some(less_than_10000(&[*a])? * 10000 + less_than_10000(tail)?),
+        [a, b, '万', tail @ ..] => {
+            Some(less_than_10000(&[*a, *b])? * 10000 + less_than_10000(tail)?)
+        }
+        [a, b, c, '万', tail @ ..] => {
+            Some(less_than_10000(&[*a, *b, *c])? * 10000 + less_than_10000(tail)?)
+        }
+        [a, b, c, d, '万', tail @ ..] => {
+            Some(less_than_10000(&[*a, *b, *c, *d])? * 10000 + less_than_10000(tail)?)
+        }
+        [a, b, c, d, e, '万', tail @ ..] => {
+            Some(less_than_10000(&[*a, *b, *c, *d, *e])? * 10000 + less_than_10000(tail)?)
+        }
+        _ => less_than_10000(s)
     }
 }
 
@@ -63,17 +86,29 @@ pub fn less_than_10000(s: &[char]) -> Option<i64> {
         ['百', c, d] => less_than_100_nun1_elided(&[*c, *d]).map(|a| 100 + a),
         ['百', c, d, e] => less_than_100(&[*c, *d, *e]).map(|a| 100 + a), // only for pure hundred
         [a, '百', tail @ ..] => {
-            let w = less_than_100_nun1_elided(&[*a])?;
-            let x = less_than_100_nun1_elided(tail)?;
-            Some(w * 100 + x)
+            Some(less_than_100_nun1_elided(&[*a])? * 100 + less_than_100_nun1_elided(tail)?)
         }
-        [a, b, '百', tail @ ..] =>  {
-            let w = less_than_100_nun1_elided(&[*a, *b])?;
-            let x = less_than_100_nun1_elided(tail)?;
-            Some(w * 100 + x)
+        [a, b, '百', tail @ ..] => {
+            Some(less_than_100_nun1_elided(&[*a, *b])? * 100 + less_than_100_nun1_elided(tail)?)
         }
         _ => less_than_100(s),
     }
 }
 
-fn positive(s: &[char]) -> Option<i64> { unimplemented!() }
+fn positive(s: &[char]) -> Option<i64> {
+    match s {
+        ['億'] => Some(100000000),
+        [head @ .., '億'] => less_than_100000000(head).map(|w| w * 100000000),
+        ['億', tail @ ..] => Some(100000000 + less_than_100000000(tail)?),
+
+        [a, '億', tail @ ..] => Some(less_than_100000000(&[*a])? * 100000000 + less_than_100000000(tail)?),
+        [a, b, '億', tail @ ..] => {
+            Some(less_than_100000000(&[*a, *b])? * 100000000 + less_than_100000000(tail)?)
+        }
+        [a, b, c, '億', tail @ ..] => {
+            Some(less_than_100000000(&[*a, *b, *c])? * 100000000 + less_than_100000000(tail)?)
+        }
+        // only need to handle till 21億
+        _ => less_than_100000000(s)
+    }
+}
